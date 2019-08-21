21ae3e8c0b1507b0c15b0e646edcb2db1c1b5630
Ken Beaton <kbeaton2@gmail.com>
2019-08-20 22:40:29 -0400

    Playing with UserCreate component

--- examples/youi/src/users/UserCreate.js
+++ examples/youi/src/users/UserCreate.js
@@ -1,137 +1,139 @@
 /* eslint react/jsx-key: off */
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import {
     Create,
-    FormTab,
     SaveButton,
-    AutocompleteInput,
-    TabbedForm,
+    DisabledInput,
+    SimpleForm,
     Toolbar,
+    TextInput,
+    DateInput,
     required,
+    minLength,
+    maxLength,
+    RadioButtonGroupInput,
+    BooleanInput,
+    crudCreate,
 } from 'react-admin';
 
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableRow from '@material-ui/core/TableRow';
-import InputLabel from '@material-ui/core/InputLabel';
-import Input from '@material-ui/core/Input';
+import { Auth } from 'aws-amplify';
 import { withStyles } from '@material-ui/core/styles';
 
-const UserEditToolbar = ({ permissions, ...props }) => (
-    <Toolbar {...props}>
-        <SaveButton
-            label="user.action.save"
-            redirect="show"
-            submitOnEnter={true}
-        />
-    </Toolbar>
-);
-
+// Create custom styles for the page.
 const styles = {
     root: {
         width: '100%',
         overflowX: 'auto',
     },
-    Table: {
-        minWidth: '100%',
+    inlineBlock: {
+        display: 'inline-flex',
+        marginRight: '1rem',
     },
-    TableRow: {
-        height: '36px',
-    },
-    Input: {
-        width: '100%',
+    toolbar: {
+        display: 'flex',
     },
 };
 
-const UserCreate = withStyles(styles)(({ classes, permissions, ...props }) => (
-    <Create {...props}>
-        <TabbedForm toolbar={<UserEditToolbar permissions={permissions} />}>
-            <FormTab label="user.form.summary" path="">
-                <Table>
-                    <TableBody>
-                        <TableRow>
-                            <TableCell>
-                                <InputLabel>First Name</InputLabel>
-                            </TableCell>
-                            <TableCell>
-                                <Input
-                                    width="100%"
-                                    id="fname"
-                                    onChange={handleChange}
-                                    autoFocus
-                                    validate={required()}
-                                />
-                            </TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>
-                                <InputLabel>Last Name</InputLabel>
-                            </TableCell>
-                            <TableCell>
-                                <Input
-                                    id="lname"
-                                    onChange={handleChange}
-                                    autoFocus
-                                    validate={required()}
-                                />
-                            </TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>
-                                <InputLabel>Email Address</InputLabel>
-                            </TableCell>
-                            <TableCell>
-                                <Input
-                                    id="email"
-                                    onChange={handleChange}
-                                    autoFocus
-                                    validate={required()}
-                                />
-                            </TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>
-                                <InputLabel>Password</InputLabel>
-                            </TableCell>
-                            <TableCell>
-                                <Input
-                                    htmlFor="component-password"
-                                    id="password"
-                                    onChange={handleChange}
-                                    autoFocus
-                                    validate={required()}
-                                />
-                            </TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>
-                                <InputLabel>Role</InputLabel>
-                            </TableCell>
-                            <TableCell>
-                                <AutocompleteInput
-                                    source=""
-                                    choices={[
-                                        { id: '', name: 'None' },
-                                        { id: 'admin', name: 'Admin' },
-                                        { id: 'user', name: 'User' },
-                                        {
-                                            id: 'user_simple',
-                                            name: 'UserSimple',
-                                        },
-                                    ]}
-                                />
-                            </TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
-            </FormTab>
-        </TabbedForm>
+// Create a custom toolbar with save (return to list) and save and continue to add users.
+const CustomToolbar = withStyles(styles)(props => (
+    <Toolbar {...props}>
+        <SaveUserButton
+            label="user.action.save"
+            redirect="list"
+            submitOnEnter={false}
+        />
+        <SaveButton
+            label="post.action.save_and_add"
+            redirect={false}
+            submitOnEnter={false}
+            variant="flat"
+        />
+    </Toolbar>
+));
+
+class SaveUserButtonComponent extends Component {
+    handleClick = () => {
+        const { basePath, handleSubmit, redirect, saveUser } = this.props;
+        console.log('Hello World');
+        return handleSubmit(values => {
+            console.log(values);
+            saveUser(values, basePath, redirect);
+        });
+    };
+
+    render() {
+        const { handleSubmitWithRedirect, saveUser, ...props } = this.props;
+
+        return (
+            <SaveButton
+                handleSubmitWithRedirect={this.handleClick}
+                {...props}
+            />
+        );
+    }
+}
+
+// Save the user through the react-admin crudCreate component.
+const saveUser = (values, basePath, redirectTo) =>
+    crudCreate('posts', { ...values }, basePath, redirectTo);
+
+const SaveUserButton = connect(
+    undefined,
+    { saveUser }
+)(SaveUserButtonComponent);
+
+const validateName = [required(), minLength(2), maxLength(15)];
+const validateEmail = [required()];
+const validateUsername = [required(), minLength(4), maxLength(12)];
+const PostCreate = withStyles(styles)(({ classes, ...props }) => (
+    <Create title={'Create User'} {...props}>
+        <SimpleForm toolbar={<CustomToolbar />}>
+            <BooleanInput label="Active Account" source="active" />
+            <DisabledInput label="Id" source="id" />
+            <TextInput
+                label="First Name"
+                source="fname"
+                validate={validateName}
+                formClassName={classes.inlineBlock}
+            />
+            <TextInput
+                source="last name"
+                validate={validateName}
+                formClassName={classes.inlineBlock}
+            />
+            <TextInput source="email" type="email" validate={validateEmail} />
+            <TextInput
+                source="username"
+                validate={validateUsername}
+                formClassName={classes.inlineBlock}
+            />
+            <TextInput
+                source="password"
+                type="password"
+                formClassName={classes.inlineBlock}
+            />
+            <TextInput
+                source="password confirm"
+                type="password"
+                formClassName={classes.inlineBlock}
+            />
+            <DateInput
+                label="Activation Date"
+                source="published_at"
+                disabled="true"
+                defaultValue={new Date()}
+            />
+            <RadioButtonGroupInput
+                label="Role"
+                source="role"
+                choices={[
+                    { id: 'user', name: 'User' },
+                    { id: 'administrator', name: 'Administrator' },
+                ]}
+            />
+        </SimpleForm>
     </Create>
 ));
 
-function handleChange(event) {
-    //setName(event.target.value);
-}
-
-export default UserCreate;
+export default PostCreate;
